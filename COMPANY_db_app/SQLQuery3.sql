fa9dc31ae380be952338220b5a6f969fdc76542b
Przemysław Pęczkowski <ppeczkowski@live.com>
2020-02-09 10:35:04 +0100

    SQL query

---
+++ COMPANY_db_app/SQLQuery3.sql
@@ -0,0 +1,26 @@
+﻿select * from employees
+SELECT * from teams
+
+
+
+--delete from employees where id >= 11 and id <= 21
+
+--INSERT INTO teams values ('Team 4')
+--DELETE FROM teams where id = 4 
+
+--USE [C:\USERS\USER\SOURCE\REPOS\COMPANY_DB_APP\COMPANY_DB_APP\COMPANY_DB_APP\BIN\DEBUG\DB_COMPANY.MDF]
+--GO
+--INSERT INTO [dbo].[employees]
+--           ([FirstName]
+--           ,[Middlename]
+--           ,[LastName]
+--           ,[Email]
+--           ,[Team_Id])
+--     VALUES
+--           ('Prz'
+--           ,NULL
+--           ,'Prz'
+--           ,'Prz'
+--           ,2)
+--GO
+
